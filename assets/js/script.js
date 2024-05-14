28f90b71e191dacfca2b21d9e21dd1c3b78c26e2
danielleboe <166262206+danielleboe@users.noreply.github.com>
2024-05-14 11:28:06 -0600

    modal form initial

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,48 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    let taskId = x + 1;
+Math.floor(Math.random() * 100000) +1;
+console.log(taskId);
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
+// function createTaskCard(task) {
 
+    // Get the modal
+var newTask = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    newTask.style.display = "block";
 }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    newTask.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == newTask) {
+    newTask.style.display = "none";
+  }
+}
+
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+
+// }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
@@ -24,11 +57,13 @@ function handleAddTask(event){
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
+    
 
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
+
 
 }
 
